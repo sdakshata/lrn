6b3c4b0f6aad90b0bf74e81e16debf7f17beb60e
Dakshata Senthil Kumar <s.dakshata@gmail.com>
2020-08-29 14:24:58 -0700

    learning variables

---
+++ python/percentage.py
@@ -0,0 +1,14 @@
+print("enter a score for math:")
+a = int(input())
+print("enter a score for english:")
+b = int(input())
+print("enter a score for science:")
+c = int(input())
+print("enter a score for band:")
+d = int(input())
+print("enter a score for baking:")
+e = int(input())
+abcde = a+b+c+d+e
+total = abcde/5
+print(total)
+
---
+++ python/temp.py
@@ -0,0 +1,4 @@
+
+c = int(input("enter a temp. in celsius:"))
+f = (c*9/5)+32
+print("the temp u gave was converted to fahrenhiet. this was our result", f)
