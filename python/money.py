dde1410eab54ffab4d67bc9ce4ee92e141bc032e
Dakshata Senthil Kumar <s.dakshata@gmail.com>
2020-09-05 15:26:50 -0700

    usd to inr

---
+++ python/money.py
@@ -0,0 +1,6 @@
+
+x = float(input("Enter some money in dollars: "))
+y = float(input("Enter the amount for the convertion: "))
+z = x*y
+print("this was the usd {} this was the convertion {} this is the answer{}".format(x,y,z))
+ 
